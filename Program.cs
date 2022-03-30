d6c8f3f8b2b3af686f0181433cdec216524014c4
ethumana <omertopcu@me.com>
2022-03-30 18:27:00 +0300

    degiskenler

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+byte b = 5; // 1 byte 0 --> 255
+
+sbyte c = 5; // 1 byte -128 -->  +127
+
+short s = 5; // 2 byte -32768 -->  +32767
+
+ushort us = 5; // 2 byte 0 --> 65365
+
+Int16 i16 = 2; // 2 byte
+
+int i = 2 ; // 4 byte -2milyar 147 milyondan artısına
+
+Int32 i32 = 2; //4 byte 
+
+Int64 i64 = 2; // 8 byte
+
+uint ui = 4;    // 4 byte
+
+long l = 4; //8byte
+
+ulong ul = 1; //8byte
+
+//reel sayılar
+float f = 5;    //4byte
+
+double d= 5;    //8byte
+
+decimal de = 5; //16 byte
+
+char c1 = '2';   //2 byte
+
+string str="Zikriye";    //sınırsız maliyetli bir veri tipi
+
+bool b1 = true;
+bool b2 = false;
+
+DateTime dt = DateTime.Now;
+
+object o1 = "x";
+object o2 = 'c';
+object o3 = 4;
+object o4 = 4.3;
+
+string str1 = string.Empty;
+str1="Zikriye Ürkmez";
+string ad = "Zikriye";
+
+
+int integer1 = 5;
+int integer2 = 3;
+int integer3 = integer1*integer2;
+
+
+bool bool1 = 10<2;
