3f5c25dcb86be9db15fc9bf65809cd9c113d6386
ethumana <omertopcu@me.com>
2022-03-31 09:18:50 +0300

    if_else_yapilari added

--- if_else_yapilari.cs
+++ if_else_yapilari.cs
@@ -5,3 +5,10 @@ else if(time<=18)
     Console.WriteLine("İyi Günler!");
 else
 Console.WriteLine("İyi Geceler");
+
+string sonuc = time <=18 ? "İyi Günler" : "İyi Geceler";
+Console.WriteLine(sonuc);
+
+sonuc = time >=6 &&time <11 ? "Günaydın" : time<=18 ? "İyi Günler!" : "İyi Geceler";
+
+Console.WriteLine(sonuc);
